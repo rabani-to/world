0db775384f75b42da144bb6213044e466d978366
D3Portillo <d3portillo@gmail.com>
2025-04-05 18:16:56 +0200

    chores: template copy works

--- packages/create-world-app/index.ts
+++ packages/create-world-app/index.ts
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 
+import fs from "fs/promises"
+import os from "os"
+import path from "path"
+import pc from "picocolors"
+import { Readable } from "stream"
+import { Command } from "commander"
+import { x } from "tar"
+
 const handleSigTerm = () => process.exit(0)
 
 process.on("SIGINT", handleSigTerm)
 process.on("SIGTERM", handleSigTerm)
 
-import { Command } from "commander"
-import pc from "picocolors"
-
 const program = new Command()
+const FOLDER =
+  "https://github.com//rabani-to/world/archive/refs/heads/master.tar.gz"
+const repo = fetch(FOLDER)
 
 program
   .name("create-world-app")
@@ -19,11 +27,62 @@ program
     "Template to use (next14, next15, vite)",
     "next15"
   )
-  .action((name, options) => {
-    console.log(pc.cyan(`\n🚀 Creating project: ${pc.bold(name)}`))
-    console.log(pc.green(`✓ Using template: ${pc.bold(options.template)}\n`))
+  .action(async (name, options) => {
+    let tmpDir = ""
+    try {
+      const tmpDir = await new Promise<string>((resolve) => {
+        const prefix = path.join(os.tmpdir(), "repo-cloned-")
+        fs.mkdtemp(prefix).then(resolve)
+      })
 
-    console.log(pc.gray("Building...\n"))
+      const buffer = await (await repo).arrayBuffer()
+      const stream = Readable.from(Buffer.from(buffer))
+
+      const files = await new Promise<string[]>((resolve) => {
+        stream
+          .pipe(
+            x({
+              strip: 1,
+              cwd: tmpDir,
+            })
+          )
+          .on("error", () => resolve([]))
+          .on("close", () =>
+            fs.readdir(tmpDir, { recursive: true }).then(resolve)
+          )
+      })
+
+      let templateName = options.template as string
+      const TEMPLATES = {
+        // name : remote slug path on github
+        next14: "template-next-14",
+        next15: "template-next-14",
+        vite: "template-next-14",
+      }
+
+      let template = TEMPLATES[templateName]
+      if (!template) {
+        // if the template is not found, default to next15
+        template = TEMPLATES.next15
+        templateName = "next15"
+      }
+
+      const templatePath = path.join(tmpDir, "packages/", template)
+      const projectPath = path.join(process.cwd(), name)
+
+      await fs.mkdir(projectPath, { recursive: true })
+      await fs.cp(templatePath, projectPath, {
+        recursive: true,
+        force: true,
+      })
+
+      console.log(pc.cyan(`\n🚀 Creating project: ${pc.bold(name)}`))
+      console.log(pc.green(`✓ Using template: ${pc.bold(options.template)}\n`))
+    } catch (_) {}
+
+    if (tmpDir) {
+      fs.rmdir(tmpDir, { recursive: true })
+    }
   })
 
 program.showHelpAfterError(true)
