0b4cf48671411bf26f1a96f629032cb4a7f7b01c
D3Portillo <d3portillo@gmail.com>
2025-04-14 00:37:54 +0200

    feat: create-world-app@alpha-5

--- packages/create-world-app/index.ts
+++ packages/create-world-app/index.ts
@@ -30,63 +30,74 @@ program
   .description("Scaffold world mini-apps faster")
   .argument("[name]", "Project name")
   .version(packageJson.version, "-v, --version", "Show package version")
-  .option("--template <template>", "Template to use (next14, next15, vite)")
-  .action(async (name, options) => {
-    intro("Let's create your mini app")
+  .option("--template <template>", "Template to use (next14, next15)")
+  .action(
+    async (name: string | undefined, options: { template?: Template }) => {
+      intro("Let's create your mini app")
 
-    if (!name) {
-      name = await text({
-        message: "Project name",
-        placeholder: "my-app",
+      if (!name) {
+        name = (await text({
+          message: "Project name",
+          placeholder: "my-app",
+        })) as any
+        if (!name) {
+          return terminateWithError("Project name is required")
+        }
+      }
+
+      if (typeof name !== "string") {
+        return terminateWithError("Invalid project name")
+      }
+
+      const tmpDir = await extractRepoFiles(repo)
+      if (!tmpDir) return terminateWithError("Failed to extract repo files")
+
+      let template = options.template
+
+      if (!template) {
+        template = (await select({
+          message: "Select a template",
+          options: [
+            { value: "next14", label: "Next 14", hint: "recommended" },
+            { value: "next15", label: "Next 15" },
+          ] satisfies Array<{
+            value: Template
+            label: string
+            hint?: string
+          }>,
+        })) as any
+      }
+
+      if (typeof template !== "string") {
+        return terminateWithError("Aborted: Invalid template")
+      }
+
+      const templateFolder = getTemplateFolder(template)
+      const templatePath = path.join(tmpDir, "packages/", templateFolder)
+      const projectPath = path.join(process.cwd(), name)
+
+      await fs.mkdir(projectPath, { recursive: true })
+      await fs.cp(templatePath, projectPath, {
+        recursive: true,
+        force: true,
       })
-      if (!name) return terminateWithError("Project name is required")
-    }
 
-    const tmpDir = await extractRepoFiles(repo)
-    if (!tmpDir) return terminateWithError("Failed to extract repo files")
+      const pkg = getPKGManager()
+      process.chdir(projectPath)
 
-    let template = options.template
+      log.step(`Using ${pc.bold(pkg)}`)
 
-    if (!template) {
-      template = await select({
-        message: "Select a template",
-        options: [
-          { value: "next15", label: "Next 15", hint: "default" },
-          { value: "next14", label: "Next 14" },
-          // TODO: vite - { value: "vite", label: "Vite" }
-        ] satisfies Array<{
-          value: Template
-          label: string
-          hint?: string
-        }>,
-      })
-    }
-
-    const templateFolder = getTemplateFolder(template)
-    const templatePath = path.join(tmpDir, "packages/", templateFolder)
-    const projectPath = path.join(process.cwd(), name)
-
-    await fs.mkdir(projectPath, { recursive: true })
-    await fs.cp(templatePath, projectPath, {
-      recursive: true,
-      force: true,
-    })
-
-    const pkg = getPKGManager()
-    process.chdir(projectPath)
-
-    log.step(`Using ${pc.bold(pkg)}`)
-
-    await tasks([
-      {
-        title: `🚀 Creating project: ${pc.green(name)}`,
-        task: async () => {
-          await install(pkg)
+      await tasks([
+        {
+          title: `🚀 Creating project: ${pc.green(name)}`,
+          task: async () => {
+            await install(pkg)
+          },
         },
-      },
-    ])
+      ])
 
-    outro(pc.greenBright(`Project created successfully!`))
-  })
+      outro(pc.greenBright(`Project created successfully!`))
+    }
+  )
   .allowUnknownOption()
   .parse(process.argv)
--- packages/create-world-app/templates.ts
+++ packages/create-world-app/templates.ts
@@ -1,10 +1,9 @@
-export const DEFAULT_TEMPLATE = "next15" as const
+export const DEFAULT_TEMPLATE = "next14" as const
 
 const TEMPLATES = {
   // shortName : https://github.com/rabani-to/world/tree/master/packages/[template]
   [DEFAULT_TEMPLATE]: "template-next-14",
-  next14: "template-next-14",
-  vite: "template-next-14",
+  next15: "template-next-15",
 } as const
 
 export type Template = keyof typeof TEMPLATES
